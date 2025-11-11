e6aa9379fc029b5f69068353b20083efa2a9e345
maksym-lototskyi <lototskyimaksym@gmail.com>
2025-11-11 22:20:45 +0100

    Cleand up main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,7 @@
 package org.example;
 
-import org.example.persistence.ObjectCollection;
-import org.example.persistence.ObjectCollectionRegistry;
-import org.example.persistence.model.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
 public class Main {
     public static void main(String[] args) {
-        /*ObjectCollection<Country> countryCollection = ObjectCollectionRegistry.getCollection(Country.class);
 
-        Country country = new Country();
-        country.setId(UUID.randomUUID());
-        country.setName("Utopia");
-
-        Capital capital = new Capital();
-        capital.setId(UUID.randomUUID());
-        capital.setName("Utopia City");
-
-        Capital capital1 = new Capital();
-        capital1.setId(UUID.randomUUID());
-        capital1.setName("New Utopia City");
-
-        country.setCapital(capital);
-        countryCollection.save(country);
-
-        country.setCapital(capital1);
-        countryCollection.save(country);*/
     }
 }
