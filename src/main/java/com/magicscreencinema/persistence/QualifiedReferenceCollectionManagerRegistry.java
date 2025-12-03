4104586285a923908b1c9033ef000268c4560285
maksym-lototskyi <lototskyimaksym@gmail.com>
2025-12-03 21:49:22 +0100

    Just changed a class from beign public to package-private

--- src/main/java/com/magicscreencinema/persistence/QualifiedReferenceCollectionManagerRegistry.java
+++ src/main/java/com/magicscreencinema/persistence/QualifiedReferenceCollectionManagerRegistry.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public class QualifiedReferenceCollectionManagerRegistry {
+class QualifiedReferenceCollectionManagerRegistry {
     private static final Map<String, QualifiedReferenceCollectionManager<?>> managers = new HashMap<>();
     static <T> QualifiedReferenceCollectionManager<T> getManager(QualifierKeyConverter<T> converter, String collectionName) {
         QualifiedReferenceCollectionManager<T> manager = new QualifiedReferenceCollectionManager<>(collectionName, converter);
