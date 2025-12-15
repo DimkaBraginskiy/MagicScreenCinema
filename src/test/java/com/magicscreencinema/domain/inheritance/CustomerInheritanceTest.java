a9bad73411bccee84fe164d15e47cb2335ec9936
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2025-12-15 19:57:35 +0100

    feat: create Person Inheritance tests

--- src/test/java/com/magicscreencinema/domain/inheritance/CustomerInheritanceTest.java
+++ src/test/java/com/magicscreencinema/domain/inheritance/CustomerInheritanceTest.java
@@ -1,4 +1,50 @@
 package com.magicscreencinema.domain.inheritance;
 
+import com.magicscreencinema.domain.enums.ContractTypeEnum;
+import com.magicscreencinema.domain.exception.NegativeValueException;
+import com.magicscreencinema.domain.model.Customer;
+import com.magicscreencinema.domain.model.Person;
+import com.magicscreencinema.domain.model.Staff;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+
 public class CustomerInheritanceTest {
+    @Test
+    public void constructor_WithLoyaltyPointsGiven_ShouldCreateCustomerOnly(){
+        Person person = new Person(
+                "Test first",
+                "Test last",
+                "756-745-33",
+                "example@gmail.com",
+                "pass",
+                LocalDate.of(1990, 10, 3),
+                150
+        );
+
+        assertTrue(person.getCustomer().isPresent());
+        assertFalse(person.getStaff().isPresent());
+
+        Customer customer = person.getCustomer().get();
+
+        assertEquals(150, customer.getLoyaltyPoints());
+        assertEquals(person, customer.getPerson());
+    }
+
+    @Test
+    public void constructor_WithNegativeLoyaltyPointsGiven_ShouldThrowException(){
+        assertThrows(NegativeValueException.class, () -> {
+            new Person(
+                    "Saul",
+                    "Goodman",
+                    "123-123-123",
+                    "saul@gmail.com",
+                    "nicepasword",
+                    LocalDate.of(1990,12,1),
+                    -10
+            );
+        });
+    }
 }
--- src/test/java/com/magicscreencinema/domain/inheritance/CustomerStaffInheritanceTest.java
+++ src/test/java/com/magicscreencinema/domain/inheritance/CustomerStaffInheritanceTest.java
@@ -1,4 +1,93 @@
 package com.magicscreencinema.domain.inheritance;
 
+import com.magicscreencinema.domain.enums.ContractTypeEnum;
+import com.magicscreencinema.domain.model.Customer;
+import com.magicscreencinema.domain.model.Person;
+import com.magicscreencinema.domain.model.Staff;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
 public class CustomerStaffInheritanceTest {
+    @Test
+    public void constructor_WithValidParameters_ShouldCreateBothInstancesAndLinkToPerson() {
+        Person person = new Person(
+                "Test first",
+                "Test last",
+                "756-745-33",
+                "example@gmail.com",
+                "pass",
+                LocalDate.of(1990, 10, 3),
+                LocalDate.of(2015, 10, 3),
+                8000,
+                ContractTypeEnum.FULL_TIME,
+                150
+        );
+
+        assertTrue(person.getStaff().isPresent());
+        assertTrue(person.getCustomer().isPresent());
+
+        Staff staff = person.getStaff().get();
+        Customer customer = person.getCustomer().get();
+
+        //Staff
+        assertEquals(LocalDate.of(2015,10,3), staff.getHireDate());
+        assertEquals(8000, staff.getSalary(), 0.0);
+        assertEquals(ContractTypeEnum.FULL_TIME, staff.getContractType());
+        assertEquals(person, staff.getPerson());
+
+        //Customer
+        assertEquals(150, customer.getLoyaltyPoints());
+        assertEquals(person, customer.getPerson());
+    }
+
+    @Test
+    public void constructor_WithValidParametersAndManager_ShouldCreateStaffCustomerAndAssignManager(){
+        Person managerPerson = new Person(
+                "Saul",
+                "Goodman",
+                "756-745-33",
+                "manager@gmail.com",
+                "pass",
+                LocalDate.of(1990, 1, 1),
+                LocalDate.of(2022, 1, 1),
+                6700,
+                ContractTypeEnum.FULL_TIME
+        );
+
+        Staff manager = managerPerson.getStaff().get();
+
+        Person employee = new Person(
+                "Test first",
+                "Test last",
+                "756-745-33",
+                "example@gmail.com",
+                "pass",
+                LocalDate.of(1990, 10, 3),
+                LocalDate.of(2015, 10, 3),
+                8000,
+                ContractTypeEnum.FULL_TIME,
+                manager,
+                150
+        );
+
+        assertTrue(employee.getStaff().isPresent());
+        assertTrue(employee.getCustomer().isPresent());
+
+        Staff staff = employee.getStaff().get();
+
+        //manager
+        assertTrue(staff.getManager().isPresent());
+        assertSame(manager, staff.getManager().get());
+
+        //bidirectional check
+        assertTrue(manager.getManagedStaff().contains(staff));
+
+        //ownership
+        assertSame(employee, staff.getPerson());
+        assertEquals(employee, employee.getCustomer().get().getPerson());
+    }
 }
